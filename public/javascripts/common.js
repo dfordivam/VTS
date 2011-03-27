72e8a0ff94c52f8ad886e1557bca625a52a5fcc9
Divam <dfordivam@gmail.com>
2011-03-27 13:49:15 +0530

    resolved total update count issue, after moving to jquery

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,8 +1,8 @@
   function update_total_count(){
-    (registration_total_bro).value = parseInt((registration_bk_bro).value) + parseInt((registration_nbk_bro).value);
-    (registration_total_sis).value = parseInt((registration_bk_sis).value) + parseInt((registration_nbk_sis).value);
-    (registration_total_teachers).value = parseInt((registration_bk_teachers).value) ;
-    (registration_grand_total).value = parseInt((registration_total_teachers).value) + parseInt((registration_total_bro).value) + parseInt((registration_total_sis).value) ;
+    $("#registration_total_bro").val(parseInt($("#registration_bk_bro").val()) + parseInt($("#registration_nbk_bro").val()));
+    $("#registration_total_sis").val(parseInt($("#registration_bk_sis").val()) + parseInt($("#registration_nbk_sis").val()));
+    $("#registration_total_teachers").val(parseInt($("#registration_bk_teachers").val()));
+    $("#registration_grand_total").val(parseInt($("#registration_total_teachers").val()) +  parseInt($("#registration_total_sis").val()) + parseInt($("#registration_total_bro").val()));
   }
 function update_count(obj, registration_count){
   if(obj.checked) {(registration_count).value = parseInt((registration_count).value) + 1 } 
