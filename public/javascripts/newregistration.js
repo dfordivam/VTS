3d9cc02b5a1574e73bd5eadc5340f6fc4c5b9b88
Divam <dfordivam@gmail.com>
2011-04-16 20:31:20 +0530

    1)New registration page: Participants list show/hide Issue Solved 2) New file for the functions required only by this page.

--- public/javascripts/newregistration.js
+++ public/javascripts/newregistration.js
@@ -1,4 +1,7 @@
-  function update_total_count(){
+// New Registration page only javascript APIs 
+//
+//
+function update_total_count(){
     $("#registration_total_bro").val(parseInt($("#registration_bk_bro").val()) + parseInt($("#registration_nbk_bro").val()));
     $("#registration_total_sis").val(parseInt($("#registration_bk_sis").val()) + parseInt($("#registration_nbk_sis").val()));
     $("#registration_total_teachers").val(parseInt($("#registration_bk_teachers").val()));
@@ -10,3 +13,13 @@ function update_count(obj, registration_count){
   update_total_count();
   }
 
+
+$(document).ready(function(){
+
+// New registration page show/hide functionality
+    $("#div_teacher").click(function(){$("#participant_category_div_teacher").slideToggle("slow");});
+    $("#div_bk_bro").click(function(){$("#participant_category_div_bk_bro").slideToggle("slow");});
+    $("#div_bk_sis").click(function(){$("#participant_category_div_bk_sis").slideToggle("slow");});
+    $("#div_nbk_bro").click(function(){$("#participant_category_div_nbk_bro").slideToggle("slow");});
+    $("#div_nbk_sis").click(function(){$("#participant_category_div_nbk_sis").slideToggle("slow");});
+    } )
