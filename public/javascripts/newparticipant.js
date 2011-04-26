34b3d4c986b0dcb29be86820854898900d921f0c
Divam <dfordivam@gmail.com>
2011-04-26 22:40:16 +0530

    JS file

---
+++ public/javascripts/newparticipant.js
@@ -0,0 +1,7 @@
+// Create New participant related JS APIs
+
+$(document).ready(function(){
+
+// More Info show/hide functionality
+    $("#see_hide_more_info").click(function(){$('div[id="more_info"]').slideToggle("slow");});
+    })
