15762862cd54781b0c0ffc34703e4a56715101db
Ravi OS <bkravi.os@gmail.com>
2011-06-13 21:22:44 +0530

    Add/Delete trains functionality added

---
+++ public/javascripts/manage_trains.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+// Manage trains show/hide feature
+
+  $("#trains_add_0").click(function(){$("#delete_train_1").show();});
+  $("#trains_add_0").click(function(){$("#add_train_1").hide();});
+  $("#trains_add_0").click(function(){$("#add_train_2").hide();});
+  $("#trains_add_0").click(function(){$("#add_train_3").hide();});
+
+  $("#trains_add_1").click(function(){$("#delete_train_1").hide();});
+  $("#trains_add_1").click(function(){$("#add_train_1").show();});
+  $("#trains_add_1").click(function(){$("#add_train_2").show();});
+  $("#trains_add_1").click(function(){$("#add_train_3").show();});
+
+    })
